5b0b23ba9682aa1eb9148598e5fbc7d6a08d6f6e
kkangyo <kangyosoon1636@gmail.com>
2021-05-15 19:28:09 +0900

    first commit

---
+++ ViewModel/iFileManager.py
@@ -0,0 +1,13 @@
+class iFileManager(object):
+    def __init__(self):
+        pass
+
+    def readDB(self):
+        pass
+
+    def readOrderList(self):
+        pass
+
+    def save(self):
+        pass
+
